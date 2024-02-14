33a02d3c7b3258db8274ef14bd88a5edcc22685f
Ahmed <ahmedeihab709@yahoo.com>
2024-02-14 14:47:50 +0200

    (feat): integrated eslint with prettier with the required dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+  },
+  extends: [
+    "next/core-web-vitals",
+    "airbnb",
+    "airbnb-typescript",
+    "plugin:prettier/recommended"
+  ],
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    "linebreak-style": 0,
+    "import/extensions": ["error", "never"],
+    "import/prefer-default-export": "off",
+    "import/no-extraneous-dependencies": ["error", { "devDependencies": false, "optionalDependencies": false, "peerDependencies": false }]
+  },
+};
